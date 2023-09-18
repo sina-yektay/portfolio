a9636d0e84fd915a48b1b70a045304dba87a6e68
Sina <sina.yektay1990@gmail.com>
2023-09-18 19:05:22 +0200

    next.config modified

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/resume',
+  assetPrefix: '/resume/',
+  webpack5: true,
+  distDir: 'out',
 }
 
 module.exports = nextConfig
